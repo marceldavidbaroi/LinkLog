e9cc2e8e6e340d108aad92d75c0f1126cb52dd65
Marcel David <marceldavidbaroi@gmail.com>
2025-08-26 23:01:39 +0600

    axios separated

---
+++ Frontend/src/api/axios.ts
@@ -0,0 +1,32 @@
+// api/axios.ts
+import axios from "axios";
+import useAuthStore from "../features/auth/store/authStore";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().clearAuth(); // optional: logout on 401
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
--- Frontend/src/features/auth/api/authApi.ts
+++ Frontend/src/features/auth/api/authApi.ts
@@ -1,20 +1,18 @@
-import axios from "axios";
-import type { LoginResponse } from "../types/auth";
+import api from "../../../api/axios";
+import type { LoginResponse, SignupResponse } from "../types/auth";
 
-const API_URL = import.meta.env.VITE_API_URL;
-console.log("API_URL:", API_URL);
 export const loginUser = async (payload: {
   username: string;
   password: string;
 }): Promise<LoginResponse> => {
-  const response = await axios.post(`${API_URL}/auth/signin`, payload);
-  return response.data;
+  const { data } = await api.post<LoginResponse>("/auth/signin", payload);
+  return data;
 };
 
 export const signupUser = async (payload: {
   username: string;
   password: string;
-}): Promise<void> => {
-  const response = await axios.post(`${API_URL}/signup`, payload);
-  return response.data;
+}): Promise<SignupResponse> => {
+  const { data } = await api.post<SignupResponse>("/auth/signup", payload);
+  return data;
 };
